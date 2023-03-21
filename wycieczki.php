e7bebf717748bcdb5d027275df138cb6aeb0f6e3
dusikowski <97994107+dusikowski@users.noreply.github.com>
2023-03-21 15:49:34 +0100

    Add files via upload

---
+++ wycieczki.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>wycieczki i urlopy</title>
+    <link href="style.css3">
+</head>
+<body>
+   <section class="baner">
+</section>
+   <section class="lewy">
+    <h1>kontakt</h1>
+    <a href="biuro@wycieczki.pl">napisz do mnie</a>
+    <p>telefon:555666777</p>
+   </section>
+    <section class="środkowy">
+<h2>GALERIA</h2>
+   </section>
+<section class="prawy">
+<h2>PROMOCJE</h2>
+<table>
+<tr>    
+    <td>jesień </td>
+    <td> grupa4+ </td>
+    <td> grupa10+ </td>
+</tr>
+<tr>
+    <td>5%</td>
+    <td>10%</td>
+    <td>15%</td>
+</tr>
+</table>
+</section>
+</body>
+</html>
